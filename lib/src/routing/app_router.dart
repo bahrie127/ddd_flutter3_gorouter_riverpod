634f7d903fc95957e7b7f66d66f6474a46cc4682
bahri <saiful.bahri.tl@gmail.com>
2022-05-22 20:12:30 +0700

    add error builder

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/product_page/product_screen.dart';
 import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
@@ -22,56 +23,58 @@ final goRouter = GoRouter(
   debugLogDiagnostics: true,
   routes: [
     GoRoute(
-        path: '/',
-        name: AppRoute.home.name,
-        builder: (context, state) => const ProductsListScreen(),
-        routes: [
-          GoRoute(
-            path: 'product/:id',
-            name: AppRoute.product.name,
-            builder: (context, state) {
-              final productId = state.params['id']!;
-              return ProductScreen(productId: productId);
-            },
+      path: '/',
+      name: AppRoute.home.name,
+      builder: (context, state) => const ProductsListScreen(),
+      routes: [
+        GoRoute(
+          path: 'product/:id',
+          name: AppRoute.product.name,
+          builder: (context, state) {
+            final productId = state.params['id']!;
+            return ProductScreen(productId: productId);
+          },
+        ),
+        GoRoute(
+          path: 'cart',
+          name: AppRoute.cart.name,
+          pageBuilder: (context, state) => MaterialPage(
+            child: const ShoppingCartScreen(),
+            key: state.pageKey,
+            fullscreenDialog: true,
           ),
-          GoRoute(
-            path: 'cart',
-            name: AppRoute.cart.name,
-            pageBuilder: (context, state) => MaterialPage(
-              child: const ShoppingCartScreen(),
-              key: state.pageKey,
-              fullscreenDialog: true,
+        ),
+        GoRoute(
+          path: 'orders',
+          name: AppRoute.orders.name,
+          pageBuilder: (context, state) => MaterialPage(
+            child: const OrdersListScreen(),
+            key: state.pageKey,
+            fullscreenDialog: true,
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          name: AppRoute.account.name,
+          pageBuilder: (context, state) => MaterialPage(
+            child: const AccountScreen(),
+            key: state.pageKey,
+            fullscreenDialog: true,
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          name: AppRoute.signIn.name,
+          pageBuilder: (context, state) => MaterialPage(
+            child: const EmailPasswordSignInContents(
+              formType: EmailPasswordSignInFormType.signIn,
             ),
+            key: state.pageKey,
+            fullscreenDialog: true,
           ),
-          GoRoute(
-            path: 'orders',
-            name: AppRoute.orders.name,
-            pageBuilder: (context, state) => MaterialPage(
-              child: const OrdersListScreen(),
-              key: state.pageKey,
-              fullscreenDialog: true,
-            ),
-          ),
-          GoRoute(
-            path: 'account',
-            name: AppRoute.account.name,
-            pageBuilder: (context, state) => MaterialPage(
-              child: const AccountScreen(),
-              key: state.pageKey,
-              fullscreenDialog: true,
-            ),
-          ),
-          GoRoute(
-            path: 'signIn',
-            name: AppRoute.signIn.name,
-            pageBuilder: (context, state) => MaterialPage(
-              child: const EmailPasswordSignInContents(
-                formType: EmailPasswordSignInFormType.signIn,
-              ),
-              key: state.pageKey,
-              fullscreenDialog: true,
-            ),
-          ),
-        ]),
+        ),
+      ],
+    ),
   ],
+  errorBuilder: (context, state) => const NotFoundScreen(),
 );
