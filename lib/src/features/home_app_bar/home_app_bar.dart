d93dbb87b2006184cd722b0a42097c176e82b963
bahri <saiful.bahri.tl@gmail.com>
2022-05-22 20:16:00 +0700

    replace gonamed to pushnamed

--- lib/src/features/home_app_bar/home_app_bar.dart
+++ lib/src/features/home_app_bar/home_app_bar.dart
@@ -50,18 +50,18 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
             ActionTextButton(
               key: MoreMenuButton.ordersKey,
               text: 'Orders'.hardcoded,
-              onPressed: () => context.goNamed(AppRoute.orders.name),
+              onPressed: () => context.pushNamed(AppRoute.orders.name),
             ),
             ActionTextButton(
               key: MoreMenuButton.accountKey,
               text: 'Account'.hardcoded,
-              onPressed: () => context.goNamed(AppRoute.account.name),
+              onPressed: () => context.pushNamed(AppRoute.account.name),
             ),
           ] else
             ActionTextButton(
               key: MoreMenuButton.signInKey,
               text: 'Sign In'.hardcoded,
-              onPressed: () => context.goNamed(AppRoute.signIn.name),
+              onPressed: () => context.pushNamed(AppRoute.signIn.name),
             ),
         ],
       );
