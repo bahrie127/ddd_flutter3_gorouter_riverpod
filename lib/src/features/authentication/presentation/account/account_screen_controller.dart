36a70564d604954b1e64102a26ae9a339289bdcf
bahri <saiful.bahri.tl@gmail.com>
2022-05-23 18:55:38 +0700

    use asyncValue guard

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -9,15 +9,10 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
   }) : super(const AsyncValue<void>.data(null));
 
   Future<bool> signOut() async {
-    try {
       state = const AsyncValue<void>.loading();
-      await authRepository.signOut();
+      state = await AsyncValue.guard(() => authRepository.signOut());
       state = const AsyncValue<void>.data(null);
-      return true;
-    } catch (e) {
-      state = AsyncValue<void>.error(e);
-      return false;
-    }
+      return state.hasError == false;
   }
 }
 
